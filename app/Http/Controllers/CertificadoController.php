1f3814fb565d09d783b6e06417ff79ec0cd1cc7f
LudSD1 <ludtp350@gmail.com>
2025-03-12 18:53:41 -0400

    actualiado 1203251850

--- app/Http/Controllers/CertificadoController.php
+++ app/Http/Controllers/CertificadoController.php
@@ -1,22 +1,22 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Models\Certificado;
-use App\Models\CertificateTemplate;
-use App\Models\Cursos;
-use App\Models\Inscritos;
-use App\Models\Progreso;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Barryvdh\DomPDF\Facade\Pdf as PDF;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
-use Illuminate\Support\Facades\File;
-use Carbon\Carbon;
-use App\Notifications\CertificadoGeneradoNotification;
-use Illuminate\Support\Facades\Cache;
+    use App\Models\Certificado;
+    use App\Models\CertificateTemplate;
+    use App\Models\Cursos;
+    use App\Models\Inscritos;
+    use App\Models\Progreso;
+    use App\Models\User;
+    use Illuminate\Http\Request;
+    use Barryvdh\DomPDF\Facade\Pdf as PDF;
+    use Illuminate\Support\Facades\Storage;
+    use Illuminate\Support\Str;
+    use SimpleSoftwareIO\QrCode\Facades\QrCode;
+    use Illuminate\Support\Facades\File;
+    use Carbon\Carbon;
+    use App\Notifications\CertificadoGeneradoNotification;
+    use Illuminate\Support\Facades\Cache;
 use Exception;
 use Illuminate\Support\Facades\Log;
 use Intervention\Image\Facades\Image;
@@ -139,85 +139,7 @@ class CertificadoController extends Controller
         ]);
     }
 
-    // public function generarCertificadoCongreso($id)
-    // {
-    //     // Buscar el curso
-    //     $curso = Cursos::findOrFail($id);
 
-    //     if ($curso->tipo != 'congreso') {
-    //         return back()->with('error', 'No se puede generar certificados.');
-    //     }
-
-    //     $plantilla = CertificateTemplate::where('curso_id', $id)->first();
-    //     if (!$plantilla) {
-    //         return back()->with('error', 'No se encontró la plantilla del certificado para este curso.');
-    //     }
-
-    //     // Obtener los inscritos que aún no tienen certificado
-    //     $inscritos = Inscritos::where('cursos_id', $id)
-    //         ->whereDoesntHave('certificado') // Verifica que no tengan un certificado aún
-    //         ->with('estudiantes')
-    //         ->get();
-
-    //     // Verificar si hay inscritos sin certificado
-    //     if ($inscritos->isEmpty()) {
-    //         return back()->with('info', 'Todos los inscritos ya tienen su certificado.');
-    //     }
-
-    //     // Recorrer cada inscrito y generar su certificado
-    //     foreach ($inscritos as $inscrito) {
-    //         // Generar código único
-    //         $codigo_certificado = Str::uuid();
-
-    //         // Generar el código QR
-    //         $qrCode = QrCode::format('svg')
-    //             ->size(200)
-    //             ->generate(route('verificar.certificado', ['codigo' => $codigo_certificado]));
-    //         $qrPath = "certificados/{$id}/qrcode_{$inscrito->id}.png";
-    //         Storage::put("public/$qrPath", $qrCode);
-
-    //         ini_set('max_execution_time', 300); // 300 segundos = 5 minutos
-
-    //         // Crear PDF del certificado personalizado
-    //         $pdf = Pdf::loadView('certificados.plantilla', [
-    //             'curso' => $inscrito->cursos->nombreCurso,
-    //             'inscrito' => $inscrito,
-    //             'codigo_certificado' => $codigo_certificado,
-    //             'firma' => public_path('storage/firmas/firmadigital.png'),
-    //             'logo' => public_path('storage/logos/logo.png'),
-    //             'fecha_emision' => now()->format('d/m/Y'),
-    //             'fecha_finalizacion' => Carbon::parse($curso->fecha_finalizacion)->format('d/m/Y'),
-    //             'qr' => $qrPath,
-    //             'tipo' => 'Congreso',
-    //             'plantillaf' => $plantilla->template_front_path,
-    //             'plantillab' => $plantilla->template_back_path,
-    //         ]);
-    //         $pdf->setOption('dpi', 300);
-
-
-    //         // Definir la ruta donde se guardará el certificado
-    //         $ruta_certificado = "certificados/{$id}/{$inscrito->id}.pdf";
-
-    //         // Guardar el archivo en el almacenamiento
-    //         Storage::put("public/$ruta_certificado", $pdf->output());
-
-    //         // Guardar los datos en la base de datos
-    //         Certificado::create([
-    //             'curso_id' => $curso->id,
-    //             'inscrito_id' => $inscrito->id,
-    //             'codigo_certificado' => $codigo_certificado,
-    //             'ruta_certificado' => $ruta_certificado,
-    //         ]);
-
-    //         if ($inscrito->estudiantes && $inscrito->estudiantes->email) {
-    //             $inscrito->estudiantes->notify(new CertificadoGeneradoNotification($inscrito, $codigo_certificado));
-    //         } else {
-    //             Log::warning("El estudiante con ID {$inscrito->estudiante_id} no tiene un correo electrónico registrado.");
-    //         }
-    //     }
-
-    //     return back()->with('success', 'Certificados generados correctamente.');
-    // }
 
     public function generarCertificadoCongreso($id)
     {
@@ -309,6 +231,15 @@ class CertificadoController extends Controller
             ->where('inscrito_id', $inscrito->id)
             ->first();
 
+                // Generar el código QR solo si no existe o si se está regenerando
+                $qrCode = QrCode::format('svg')
+                ->size(200)
+                ->generate(route('verificar.certificado', ['codigo' => $codigo_certificado]));
+
+            // Guardar el código QR en el almacenamiento
+                $qrPath = "certificados/{$inscrito->cursos_id}/qrcode_{$inscrito->id}.svg";
+                Storage::put("public/$qrPath", $qrCode);
+
         // Si ya existe, usamos el código existente
         if ($certificadoExistente) {
             $codigo_certificado = $certificadoExistente->codigo_certificado;
@@ -326,14 +257,7 @@ class CertificadoController extends Controller
             ]);
         }
 
-        // Generar el código QR solo si no existe o si se está regenerando
-        $qrCode = QrCode::format('svg')
-        ->size(200)
-        ->generate(route('verificar.certificado', ['codigo' => $codigo_certificado]));
 
-    // Guardar el código QR en el almacenamiento
-        $qrPath = "certificados/{$inscrito->cursos_id}/qrcode_{$inscrito->id}.svg";
-        Storage::put("public/$qrPath", $qrCode);
 
         // Enviar notificación con el link de verificación solo si no se está regenerando
         if (!$regenerando && $inscrito->estudiantes && $inscrito->estudiantes->email) {
@@ -345,99 +269,7 @@ class CertificadoController extends Controller
     }
 
 
-    //     if (!auth()->check()) {
-    //         return redirect()->route('login')->with('error', 'Debes iniciar sesión para generar el certificado.');
-    //     }
-    //     $Cursoid = decrypt($Cursoid);
-    //     // Buscar el curso
-    //     $curso = Cursos::findOrFail($Cursoid);
-
-
-
-
-    //     // Verificar que el curso sea de tipo "congreso"
-    //     if ($curso->tipo != 'congreso') {
-    //         return back()->with('error', 'No se puede generar certificados para este tipo de curso.');
-    //     }
-
-    //     if ($curso->estado != 'Certificado Disponible') {
-    //         return back()->with('error', 'El certificado no esta Disponible.');
-    //     }
-
-    //     // Verificar que el usuario esté inscrito en el curso
-    //     $inscrito = Inscritos::where('cursos_id', $Cursoid)
-    //         ->where('estudiante_id', auth()->user()->id)
-    //         ->first();
-
-    //     if (!$inscrito) {
-    //         return back()->with('error', 'No estás inscrito en este curso.');
-    //     }
-
-    //     // Verificar que el usuario no haya generado un certificado previamente
-    //     $certificadoExistente = Certificado::where('curso_id', $Cursoid)
-    //         ->where('inscrito_id', $inscrito->id)
-    //         ->first();
-
-    //     if ($certificadoExistente) {
-    //         return back()->with('error', 'Ya has generado un certificado para este curso.');
-    //     }
-
-    //     // Verificar que exista una plantilla de certificado
-    //     $plantilla = CertificateTemplate::where('curso_id', $Cursoid)->first();
-    //     if (!$plantilla) {
-    //         return back()->with('error', 'No se encontró la plantilla del certificado para este curso.');
-    //     }
-
-    //     // Generar código único
-    //     $codigo_certificado = Str::uuid();
-
-    //     // Generar el código QR
-    //     $qrCode = QrCode::format('png')
-    //         ->size(200)
-    //         ->generate(route('verificar.certificado', ['codigo' => $codigo_certificado]));
-    //     $qrPath = "certificados/{$Cursoid}/qrcode_{$inscrito->id}.png";
-    //     Storage::put("public/$qrPath", $qrCode);
-
-    //     // Crear PDF del certificado personalizado
-    //     $pdf = Pdf::loadView('certificados.plantilla', [
-    //         'curso' => $curso->nombreCurso,
-    //         'inscrito' => $inscrito,
-    //         'codigo_certificado' => $codigo_certificado,
-    //         'firma' => public_path('storage/firmas/firmadigital.png'),
-    //         'logo' => public_path('storage/logos/logo.png'),
-    //         'fecha_emision' => now()->format('d/m/Y'),
-    //         'fecha_finalizacion' => Carbon::parse($curso->fecha_finalizacion)->format('d/m/Y'),
-    //         'qr' => $qrPath,
-    //         'tipo' => 'Congreso',
-    //         'plantillaf' => $plantilla->template_front_path,
-    //         'plantillab' => $plantilla->template_back_path,
-    //     ]);
-    //     $pdf->setOption('dpi', 300);
-
-    //     // Definir la ruta donde se guardará el certificado
-    //     $ruta_certificado = "certificados/{$Cursoid}/{$inscrito->id}.pdf";
-
-    //     // Guardar el archivo en el almacenamiento
-    //     Storage::put("public/$ruta_certificado", $pdf->output());
-
-    //     // Guardar los datos en la base de datos
-    //     Certificado::create([
-    //         'curso_id' => $curso->id,
-    //         'inscrito_id' => $inscrito->id,
-    //         'codigo_certificado' => $codigo_certificado,
-    //         'ruta_certificado' => $ruta_certificado,
-    //     ]);
-
-    //     // Notificar al estudiante
-    //     if ($inscrito->estudiantes && $inscrito->estudiantes->email) {
-    //         $inscrito->estudiantes->notify(new CertificadoGeneradoNotification($inscrito, $codigo_certificado));
-    //     } else {
-    //         Log::warning("El estudiante con ID {$inscrito->estudiante_id} no tiene un correo electrónico registrado.");
-    //     }
-
-    //     return back()->with('success', 'Certificado generado correctamente.');
-    // }
-
+        
 
     public function verificarCertificado($codigo)
     {
--- resources/views/Docente/AsignarAsistencia.blade.php
+++ resources/views/Docente/AsignarAsistencia.blade.php
@@ -2,74 +2,8 @@
     Dar Asistencia
 @endsection
 
-@section('nav')
-    @if (auth()->user()->hasRole('Administrador'))
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('Miperfil') }}">
-                    <i class="ni ni-circle-08 text-green"></i> Mi perfil
-                </a>
-            </li>
-            <li class="nav-item   ">
-                <a class="nav-link   " href="{{ route('Inicio') }}">
-                    <i class="ni ni-tv-2 text-primary"></i> Mis Cursos
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="{{ route('ListaDocentes') }}">
-                    <i class="ni ni-single-02 text-blue"></i> Lista de Docentes
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="{{ route('ListaEstudiantes') }}">
-                    <i class="ni ni-single-02 text-orange"></i> Lista de Estudiantes
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="{{ route('aportesLista') }}">
-                    <i class="ni ni-bullet-list-67 text-red"></i> Aportes
-                </a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link active" href="{{ route('AsignarCurso') }}">
-                    <i class="ni ni-key-25 text-info"></i> Asignación de Cursos
-                </a>
-            </li>
 
-        </ul>
-    @endif
-
-    @if (auth()->user()->hasRole('Docente'))
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('Miperfil') }}">
-                    <img src="{{ asset('assets/icons/user.png') }}" alt="Mi perfil Icon"
-                        style="width: 16px; margin-right: 10px;">
-                    Mi perfil
-                </a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link active" href="{{ route('Inicio') }}">
-                    <img src="{{ asset('assets/icons/cursos.png') }}" alt "cursos" style="width: 16px; margin-right: 10px;">
-                    Mis Cursos
-                </a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="{{ route('pagos') }}">
-                    <img src="{{ asset('assets/icons/pago.png') }}" alt "pago" style="width: 16px; margin-right: 10px;">
-                    Mis Aportes
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('AsignarCurso') }}">
-                    <img src="{{ asset('assets/icons/asignar.png') }}" alt="Asignar Cursos Icon"
-                        style="width: 16px; margin-right: 10px;">
-                    Asignación de Cursos
-                </a>
-            </li>
-        </ul>
-    @endif
-@endsection
+        
 
 
 @section('content')
